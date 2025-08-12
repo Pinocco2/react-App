70d8f7c2734a5d809441a7b24cb33ed82602839c
Стефан <bukotko2009@gmail.com>
2025-08-12 14:32:21 +0300

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Greeting from './components/Greeting';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Greeting name="Стефан"/>
+
+    </div>
+  );
+}
+
+export default App
